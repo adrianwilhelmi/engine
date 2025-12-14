309ac6025466a3e68332c0b872ce32237cd1ddd6
adrian <adrian@debian.adrian>
2025-12-14 15:03:36 +0100

    added vulkan dependency

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include<iostream>
 #include<memory>
 #include<cassert>
+#include<vector>
 
 #include<SDL3/SDL.h>
+#include<SDL3/SDL_vulkan.h>
+
+#include<vulkan/vulkan.h>
 
 struct SdlDeleter{
 	void operator()(SDL_Window*p) const {SDL_DestroyWindow(p);}
@@ -29,7 +33,43 @@ int main(){
 		return 1;
 	}
 
+
+
+	Uint32 ext_count = 0;
+	const char* const* instance_exts = SDL_Vulkan_GetInstanceExtensions(&ext_count);
+
+	if(instance_exts == NULL){
+			std::cerr << "instance exts is null: " << SDL_GetError() << std::endl;
+			return 1;
+	}
+
+	std::vector<const char*> extensions(instance_exts, instance_exts + ext_count);
+
+	// vulkan instance
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "SDL3 Vulkan Test";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+	appInfo.pEngineName = "CustomEngine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+	appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+    VkInstance instance;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Vulkan instance created successfully" << std::endl;
+
 	assert(pwindow != nullptr);
+
 
 	bool running = true;
 	SDL_Event e;
@@ -39,6 +79,7 @@ int main(){
 		}
 	}
 
+	vkDestroyInstance(instance, nullptr);
 	SDL_Quit();
 
 	return 0;
