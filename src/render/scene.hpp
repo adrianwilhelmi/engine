1fb69d672d5a95cb182cabb3bdfcca4642e8f865
adrian <adrian@debian.adrian>
2026-09-20 12:46:26 +0200

    example to render

--- src/render/scene.hpp
+++ src/render/scene.hpp
@@ -5,13 +5,14 @@
 
 #include<render/camera_data.hpp>
 #include<render/render_object.hpp>
+#include<render/color.hpp>
 
 namespace engine::render {
 
 struct Scene{
 	CameraData camera;
 	std::vector<RenderObject> objects;
-	uint32_t clear_color = 0xFF000000;
+	uint32_t clear_color = pack_argb8888(engine::render::Color::black());
 };
 
 } // namespace engine::render
